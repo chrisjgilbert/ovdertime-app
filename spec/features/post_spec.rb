d38b0315ce97f5d5ae5954a4b26602bc05c3e0d9
Chris Gilbert <chrisgilbert4@icloud.com>
2019-02-15 12:46:22 +0000

    posts new/create implemented

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -4,12 +4,31 @@ describe 'navigate' do
   describe 'index' do
     it 'can be reached successfully' do
       visit posts_path
+
       expect(page.status_code).to eq 200
     end
 
     it 'has a title of posts' do
       visit posts_path
+
       expect(page).to have_content 'Post'
+    end
+  end
+
+  describe 'creation' do
+    it 'has a new form that can be reached' do
+      visit new_post_path
+
+      expect(page.status_code).to eq 200
+    end
+
+    it 'can be created from new for page' do
+      visit new_post_path
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: 'Some rationale'
+      click_on 'save'
+
+      expect(page).to have_content('Some rationale')
     end
   end
 end
