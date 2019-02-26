9cf67c1cc3dba8d08df217b78164c31a22ac23e1
Chris Gilbert <chrisgilbert4@icloud.com>
2019-02-26 15:53:38 +0000

    posts/index view implemented

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 describe 'navigate' do
+  before do
+    user = User.create(first_name: "Jon", last_name: "Snow", email: "test@test.com", password: "secret", password_confirmation: "secret")
+    login_as(user, :scope => :user)
+  end
   describe 'index' do
     before do
       visit posts_path
@@ -10,14 +14,19 @@ describe 'navigate' do
     end
 
     it 'has a title of posts' do
-      expect(page).to have_content 'Post'
+      expect(page).to have_content(/Posts/)
+    end
+
+    it 'has a list of posts' do
+      post1 = Post.create(date: Date.today, rationale: 'Post1')
+      post2 = Post.create(date: Date.today, rationale: 'Post2')
+      visit posts_path
+      expect(page).to have_content(/Post1|Post2/)
     end
   end
 
   describe 'creation' do
     before do
-      user = User.create(first_name: "Jon", last_name: "Snow", email: "test@test.com", password: "secret", password_confirmation: "secret")
-      login_as(user, :scope => :user)
       visit new_post_path
     end
     it 'has a new form that can be reached' do
