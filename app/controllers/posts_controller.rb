2f3e1b7852602d6ecd08cd522975dfdf6fe6ed3d
Chris Gilbert <chrisgilbert4@icloud.com>
2019-02-28 17:53:42 +0000

    delete post implemented

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
 
-  before_action :set_post, only: [:show, :edit, :update]
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
     @posts = Post.all
@@ -20,7 +20,7 @@ class PostsController < ApplicationController
     end
   end
 
-  def edit  
+  def edit
   end
 
   def update
@@ -33,6 +33,11 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
+  end
+
+  def destroy
+    @post.delete
+    redirect_to posts_path, notice: 'Your post was deleted successfully'
   end
 
   private
